b47df657ba46859801edae5247fd61e5045ad546
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-26 00:40:36 +0200

    fixed

--- lesson3/hometask2/src/Profile.jsx
+++ lesson3/hometask2/src/Profile.jsx
@@ -5,10 +5,12 @@ const Profile = props => {
   const formatDate = moment(props.user.birthDate).format('DD MMM YYYY');
 
   return (
-    <div className="container">
+    // <div className="container">
+    <>
       <div className="profile__name">{`${props.user.firstName} ${props.user.lastName}`}</div>
       <div className="profile__birth">{`Was born ${formatDate} in ${props.user.birthPlace}`}</div>
-    </div>
+    </>
+    // </div>
   );
 };
 
