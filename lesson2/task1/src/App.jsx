1986000ddaa52efcb8c9c6c1517c797f39094ea1
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-25 11:37:10 +0200

    solution less2 task1

---
+++ lesson2/task1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Search Form</h1>
+        <div>
+          <input type="text" />
+          <button>Search</button>
+          </div>
+    </>
+  )
+ 
+};
+
+export default App;
