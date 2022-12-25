da6b0ab670f809339922fedc63f3307a56d03f1f
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-25 22:49:32 +0200

    fixed

--- lesson3/hometask1/src/App.jsx
+++ lesson3/hometask1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Greeting from './Greeting';
 
-const App = () => <Greeting lastName="Doe" firstName="John" birthDate="20 Dec 2000" />;
+const App = () => <Greeting firstName="John" lastName="Doe" birthDate={new Date('20 Dec 2000')} />;
 
 export default App;
