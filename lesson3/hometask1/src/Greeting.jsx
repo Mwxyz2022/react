56a75ead8f2be3c09ab6db305d9aa9d3254132bc
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-26 21:37:45 +0200

    fixed

--- lesson3/hometask1/src/Greeting.jsx
+++ lesson3/hometask1/src/Greeting.jsx
@@ -5,7 +5,7 @@ const Greeting = props => {
   const yearsOld = moment().diff(props.birthDate, 'years');
 
   return (
-    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${yearsOld} years old`}</div>
+    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${yearsOld} years old`}</div>
   );
 };
 
