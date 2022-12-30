218d47a3014b809f1dbeb9036d6643df07c0c0ad
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-30 17:49:40 +0200

    fix

---
+++ lesson5/hometask2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+
+const App = () => (
+  <>
+    <ColorPicker />
+  </>
+);
+
+export default App;
---
+++ lesson5/hometask2/src/ColorPicker.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const AQUA = 'Aqua';
+const CORAL = 'Coral';
+const BISQUE = 'Bisque';
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colorName: null,
+    };
+  }
+
+  over = colorName => {
+    this.setState({
+      colorName,
+    });
+  };
+
+  out = () => {
+    this.setState({
+      colorName: null,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div class="picker__title">{this.state.colorName}</div>
+        <div>
+          <button
+            class="picker__button picker__button_coral"
+            onMouseOver={() => this.over(CORAL)}
+            onMouseOut={() => this.out(CORAL)}
+          ></button>
+          <button
+            class="picker__button picker__button_aqua"
+            onMouseOver={() => this.over(AQUA)}
+            onMouseOut={() => this.out(CORAL)}
+          ></button>
+          <button
+            class="picker__button picker__button_bisque"
+            onMouseOver={() => this.over(BISQUE)}
+            onMouseOut={() => this.out(CORAL)}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
