f40c5ca4ae8bc9fa16a9aab4b423c93c59329322
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-07 20:43:44 +0200

    solution hometask

---
+++ lesson7/hometask1/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import TransactionsList from './TransactionsList.jsx';
+
+const App = props => <TransactionsList transactions={props.transactions} />;
+
+export default App;
