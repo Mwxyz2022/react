598f73a191f444e359b698d58baf6deeebe6ed72
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-12 20:47:42 +0200

    silution task

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ShoppingCart from './ShoppingCart';
+import Profile from './Profile';
 
-const App = () => <div>Hello, React!</div>;
+class Page extends Component {
+  state = {
+    userDate: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+  };
 
-export default App;
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      userDate: {
+        ...this.state.userDate,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userDate } = this.state;
+    return (
+      <div className="page">
+        <h1 className="title">{`Hello, ${userDate.firstName} ${userDate.lastName}`}</h1>
+        <main className="content">
+          <ShoppingCart userName={userDate.firstName} />
+          <Profile userDate={userDate} handleChange={this.handleChange} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Page;
