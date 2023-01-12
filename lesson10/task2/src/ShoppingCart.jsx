ad0546204a0ad425ee11e5f23a70f2aaab2f3888
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-12 20:53:34 +0200

    fixed

--- lesson10/task2/src/ShoppingCart.jsx
+++ lesson10/task2/src/ShoppingCart.jsx
@@ -20,7 +20,7 @@ class ShoppingCart extends Component {
 
   render() {
     const count = this.state.cartItems.length;
-    console.log(this.state.cartItems);
+
     return (
       <div className="column">
         <CartTitle userName={this.props.userName} count={count} />
