42c1c7a737237991eff6c9671dbf8ba8f1d6a49b
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-19 16:38:56 +0200

    solution task1

--- lesson11/task1/src/App.jsx
+++ lesson11/task1/src/App.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Numbers from './Numbers';
+import EvenNumbers from './EvenNumbers';
+import OddNumbers from './OddNumbers';
 
-const App = () => <div>Hello, React!</div>;
+class App extends Component {
+  state = {
+    number: 0,
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        number: this.state.number + 1,
+      });
+    }, 1000);
+    console.log(this.intervalId);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    console.log(this.intervalId);
+    return (
+      <div className="app">
+        <OddNumbers title="Odd numbers" number={this.state.number} />
+        <EvenNumbers title="Even numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={5} />
+      </div>
+    );
+  }
+}
 
 export default App;
