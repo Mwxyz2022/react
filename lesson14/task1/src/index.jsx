a8129963509848661065043da3622b6efc94f3f6
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-24 16:23:03 +0200

    fixed

--- lesson14/task1/src/index.jsx
+++ lesson14/task1/src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import Counter from './Counter'
 
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector('#root')
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Counter />, rootElement)
